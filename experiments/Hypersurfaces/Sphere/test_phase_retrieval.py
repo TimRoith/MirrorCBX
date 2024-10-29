45170a57226e10437b194a148321d24d6a5c7b80
Tim Roith <tim.roith@desy.de>
2024-10-29 01:55:48 +0100

    add exp

--- experiments/Hypersurfaces/Sphere/test_phase_retrieval.py
+++ experiments/Hypersurfaces/Sphere/test_phase_retrieval.py
@@ -4,7 +4,7 @@ from experiment_setup import PhaseRetrieval_Experiment
 conf = PhaseRetrieval_Experiment('params/mirror_params_phase.yaml')
 
 for M in [500, 1000]:
-    num_runs = 2
+    num_runs = 20
     success = 0
     #%%
     for run in range(num_runs):
@@ -17,6 +17,8 @@ for M in [500, 1000]:
         
         ev = conf.eval_run(dyn)
         success += ev['success']
+        print('Run M= ' + str(M) + ', num success= ' + str(success) + ' dist= '
+              + str(ev['consensus_diff'][-1]))
     #%%
     print(30*'<>-<>')
     print('Run M= ' + str(M))
