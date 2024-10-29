e713f9bb1aa3da7d96590a79e8ae09437aee16e6
TimRoith <tim.roith@fau.de>
2024-10-29 01:54:57 +0100

    added phase retrieval

--- experiments/Hypersurfaces/Sphere/test_phase_retrieval.py
+++ experiments/Hypersurfaces/Sphere/test_phase_retrieval.py
@@ -1,14 +1,14 @@
 from experiment_setup import PhaseRetrieval_Experiment
 
 
-conf = PhaseRetrieval_Experiment('params/mirror_params_phase.yaml')
 
-for M in [500, 1000]:
+pre = 'params/mirror_params_phase'
+for conf_name in [pre +'500.yaml']:
     num_runs = 2
     success = 0
+    conf = PhaseRetrieval_Experiment(conf_name)
     #%%
     for run in range(num_runs):
-        conf.config.problem.M = M
         xin = conf.init_x()
         dyn = conf.dyn_cls(conf.get_objective(), x = xin, **conf.dyn_kwargs)
         
@@ -19,5 +19,5 @@ for M in [500, 1000]:
         success += ev['success']
     #%%
     print(30*'<>-<>')
-    print('Run M= ' + str(M))
+    print('Run M= ' + str(conf.config.problem.M))
     print('Success Rate: ' + str(success/num_runs))
